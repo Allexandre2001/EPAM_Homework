4e40df6904d87fac32a51925049f81756cf50582
Alex <tarabanov804@gmail.com>
2022-11-13 21:05:16 +0000

    Bah_1

--- A.sh
+++ A.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-# if [$1]; then
-# echo " "
-# fi
 if [ "$1" == "" ]; then
-echo "dfhdfh"
+echo "Choose"
 elif [ "$1" == "--all" ]; then
 nmap -sL 192.168.0.1/24
 elif [ "$1" == "--target" ]; then
